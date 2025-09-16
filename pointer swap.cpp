c228712a8548e635b05770eca93b0515821464f0
shantanujoshi136 <shantanujoshitech@gmail.com>
2025-09-16 10:48:05 +0530

    Add files via upload

---
+++ pointer swap.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int swp(int*a , int*b){
+int t;
+t=*a;
+*a=*b;
+*b=t;
+printf("Value after swap");
+printf("\na: %d\nb: %d",*a,*b);
+}
+void main()
+{
+ 	 int a,b;
+	 printf("Enter two value : ");
+	 scanf("%d %d",&a,&b);
+	 swp(&a,&b);
+}
